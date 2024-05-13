ff420308d7fe0ab2d78cb0eff42520d016737bf0
iLOVEdisney2023 <mialgalong@hotmail.com>
2024-05-13 19:30:33 -0400

    TP terminé

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("allo")
+
+let images= document.querySelectorAll(".grille img");
+document.querySelector(".modale").addEventListener("click",FermerModale);
+
+for(let i of images){
+    i.addEventListener("click",OuvrirModale);
+} 
+
+function OuvrirModale(){
+
+    let image=this.getAttribute("src"); 
+   
+    let element=document.createElement("img");
+    element.setAttribute('src',image);
+
+    document.querySelector(".modale").appendChild(element);
+
+    document.querySelector(".modale").classList.add("actif");
+}
+
+function FermerModale(){
+    let image = document.querySelector('.modale img');
+
+    document.querySelector('.modale').removeChild(image);
+    document.querySelector(".modale").classList.remove("actif");
+}
+    
